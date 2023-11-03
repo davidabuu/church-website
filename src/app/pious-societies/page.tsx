d237b24b0ed2e803c9faf388d1b6d91909abe75e
davidabuu <davidabu80@gmail.com>
2023-11-03 15:12:29 +0100

    UI Better

--- src/app/pious-societies/page.tsx
+++ src/app/pious-societies/page.tsx
@@ -2,10 +2,7 @@
 import PiousSocieties from "@/components/PiousSocieties";
 import { Metadata } from "next";
 import React from "react";
-export const metadata: Metadata = {
-  title: "Pious Societies | Annunctiation Parish",
-  description: "",
-};
+
 const Page = () => {
   return (
     <div>
